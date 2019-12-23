474ef2f51960914e439e858d461b0047f10469cd
mikael autheman <mika@atoomik.fr>
2019-12-23 19:48:01 +0100

    first page home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @characters = Character.all
+    @gods = God.all
+    @places = Place.all
   end
 end
