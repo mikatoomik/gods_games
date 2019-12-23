05897695d7a370bd480a54af8986b082245f92e7
mikael autheman <mika@atoomik.fr>
2019-12-23 23:43:15 +0100

    create new character

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,18 @@ class PagesController < ApplicationController
     @characters = Character.all
     @gods = God.all
     @places = Place.all
+    place = Place.last
+    combat_init(place)
+    combat_result(@advs)
+  end
+
+  def combat_init(place)
+    @adv1 = Character.where("place_id = ?", place).first
+    @adv2 = Character.where("place_id = ?", place).last
+    @advs = [@adv1, @adv2]
+  end
+
+  def combat_result(advs)
+    @winner = advs.sample
   end
 end
