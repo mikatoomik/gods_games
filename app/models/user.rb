42ea06f89cbb61f56d2714d31560992ab8205c5b
mikael autheman <mika@atoomik.fr>
2019-12-28 15:52:37 +0100

    test job fake

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :characters
+
+  after_commit :async_update # Run on create & update
+
+  private
+
+  def async_update
+    UpdateUserJob.perform_later(self.id)
+  end
 end
